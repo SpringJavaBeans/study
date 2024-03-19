c5beb7ea2d0b6b440f08d753d00fb56d6bf85a5a
parksujin <69441691+corazon4815@users.noreply.github.com>
2024-03-19 23:12:21 +0900

    Update 위장.java

--- sujin/위장.java
+++ sujin/위장.java
@@ -6,10 +6,12 @@ class Solution {
         int answer = 0;
         Map<String, Integer> map = new HashMap<>();
         for(int i = 0; i< clothes.length; i++) {
+            //착용 안하는 경우를 포함해서 기본값 1 
             map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 1) + 1);
         }
         Collection<Integer> values = map.values();
         answer = values.stream().reduce(1, (a,b) -> a*b);
+        //모든 아이템을 착용 안하는 경우 -1
         return answer-1;
     }
 }
