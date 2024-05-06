e0c56dbb48328d7f8e1cd8fcd23af398ba857439
wdmab1204 <wdmab1204@naver.com>
2024-05-06 14:23:19 +0900

    박건우 - 기지국 설치(cpp)

---
+++ gunwoo/기지국 설치.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(int n, vector<int> stations, int w)
+{
+    int answer = 0;
+    vector<int> emptyAreaList;
+    int oldStation = stations[0];
+    if (oldStation - w - 1 > 0)
+        emptyAreaList.push_back(oldStation - w - 1);
+
+    for (int i = 1; i < stations.size(); i++) {
+        int station = stations[i];
+        if ((station - w) - (oldStation + w) - 1 > 0) {
+            emptyAreaList.push_back((station - w) - (oldStation + w) - 1);
+        }
+
+        oldStation = station;
+    }
+
+    if (oldStation + w < n)
+        emptyAreaList.push_back(n - oldStation - w);
+
+    int range = w * 2 + 1;
+    for (int k = 0; k < emptyAreaList.size(); k++) {
+        int emptyArea = emptyAreaList[k];
+        int a = emptyArea / range;
+        if (emptyArea % range != 0) a++;
+        answer += a;
+    }
+
+    return answer;
+}
